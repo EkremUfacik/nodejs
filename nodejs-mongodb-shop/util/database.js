7107e4dfc29424b8559449238ae9c2bbf7fbe3a5
ekrem <ekremufacik07@gmail.com>
2023-07-09 16:35:58 +0300

    first commit

---
+++ nodejs-mongodb-shop/util/database.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+/** @type {mongodb.Db} */
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(process.env.MONGO_DB)
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => console.log(err));
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
